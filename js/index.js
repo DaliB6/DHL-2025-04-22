1c4f597ce54a4a3d5154ac2afe5b69e2873e8dd5
champix56 <desqorbaix@free.fr>
2025-04-23 09:58:47 +0200

    declenchement par event de chargement

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 
-function loadJs() {
+function loadJs(evt) {
+    // console.log(arguments);
+    console.log(evt);
     var jsLoaded = document.querySelector('#js-loaded');
     jsLoaded.innerHTML = "JS OK";
+    jsLoaded.style.backgroundColor = "skyblue";
+    jsLoaded.style.color = "tomato";
+    jsLoaded.remove();
 }
-loadJs();
+
+document.addEventListener('DOMContentLoaded', loadJs);
