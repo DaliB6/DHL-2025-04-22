2052b954db44a42575183b18fcbb111fcf013ef2
champix56 <desqorbaix@free.fr>
2025-04-23 11:35:45 +0200

    demo class

---
+++ js/Images.js
@@ -0,0 +1,27 @@
+/**
+ * class pour la manipulation du REST
+ */
+class Images {
+    //ici je met l'url des ressources sur le srveur REST pour y acceder
+    ressourcePath;
+    /**
+     * constructeur d'images
+     * @param {string} ressourcePath chemin dans le REST des images
+     */
+    constructor(ressourcePath) {
+        this.ressourcePath = ressourcePath;
+    }
+    /**
+     * remplacement d'image
+     * @param {object} origineImage image a remplacer
+     * @param {object} newImage image de substitution
+     * @returns {object} image remplacé
+     */
+    replaceImage(origineImage, newImage) {
+        return origineImage;
+    }
+}
+/**
+ * instance principales de toutes les images de l'app
+ */
+const images = new Images();
