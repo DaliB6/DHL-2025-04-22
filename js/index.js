25d17a851597606e640b30428f1908276f7deea6
champix56 <desorbaix@free.fr>
2025-04-24 21:47:50 +0200

    chargement vue list

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import { loadEditor } from "./editor.js";
+import { loadList } from "./List.js";
 
 function loadJs(evt) {
   console.log(evt);
@@ -7,25 +8,27 @@ function loadJs(evt) {
   jsLoaded.style.backgroundColor = "skyblue";
   jsLoaded.style.color = "tomato";
   jsLoaded.remove();
-  document.querySelector("#contentA1").addEventListener("click", (evt) => {
-    evt.stopPropagation();
-    console.log("click-A1", evt.target);
-  });
-  document.querySelector("#container").addEventListener("click", (evt) => {
-    console.log("clickContainer", evt.target, evt.currentTarget);
-  });
 
   document
     .querySelector("#navbarNav > ul > li:nth-child(2) > a")
     .addEventListener("click", (evt) => {
-        evt.preventDefault();
+      evt.preventDefault();
       loadEditorView();
     });
 
   // loadHome();
   //loadEditorView();
+  //loadListView();
 }
-function loadHomeView() {
+function loadListView() {
+  fetch("/vues/List.html")
+  .then((r) => r.text())
+  .then((h) => {
+    document.querySelector("main").innerHTML = h;
+    loadList();
+  });
+}
+  function loadHomeView() {
   fetch("/vues/home.html")
     .then((r) => r.text())
     .then((h) => {
@@ -40,4 +43,5 @@ function loadEditorView() {
       loadEditor();
     });
 }
+
 document.addEventListener("DOMContentLoaded", loadJs);
