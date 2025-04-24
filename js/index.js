fea90bc6c65126039e22f1ae9d01f89e34eae58e
champix56 <desqorbaix@free.fr>
2025-04-24 17:33:29 +0200

    FIN J3

--- js/index.js
+++ js/index.js
@@ -1,14 +1,43 @@
-import {loadEditor} from './editor.js'
+import { loadEditor } from "./editor.js";
 
 function loadJs(evt) {
-    // console.log(arguments);
-    console.log(evt);
-    var jsLoaded = document.querySelector('#js-loaded');
-    jsLoaded.innerHTML = "JS OK";
-    jsLoaded.style.backgroundColor = "skyblue";
-    jsLoaded.style.color = "tomato";
-    jsLoaded.remove();
-    loadEditor();
-}
+  console.log(evt);
+  const jsLoaded = document.querySelector("#js-loaded");
+  jsLoaded.innerHTML = "JS OK";
+  jsLoaded.style.backgroundColor = "skyblue";
+  jsLoaded.style.color = "tomato";
+  jsLoaded.remove();
+  document.querySelector("#contentA1").addEventListener("click", (evt) => {
+    evt.stopPropagation();
+    console.log("click-A1", evt.target);
+  });
+  document.querySelector("#container").addEventListener("click", (evt) => {
+    console.log("clickContainer", evt.target, evt.currentTarget);
+  });
 
-document.addEventListener('DOMContentLoaded', loadJs);
+  document
+    .querySelector("#navbarNav > ul > li:nth-child(2) > a")
+    .addEventListener("click", (evt) => {
+        evt.preventDefault();
+      loadEditorView();
+    });
+
+  // loadHome();
+  //loadEditorView();
+}
+function loadHomeView() {
+  fetch("/vues/home.html")
+    .then((r) => r.text())
+    .then((h) => {
+      document.querySelector("main").innerHTML = h;
+    });
+}
+function loadEditorView() {
+  fetch("/vues/Editor.html")
+    .then((r) => r.text())
+    .then((h) => {
+      document.querySelector("main").innerHTML = h;
+      loadEditor();
+    });
+}
+document.addEventListener("DOMContentLoaded", loadJs);
