ca8c9bf8a4316f8564ff8cb3a0bffbc2ba9cc6b1
champix56 <desorbaix@free.fr>
2025-04-24 21:31:53 +0200

    gestion js de template de list

---
+++ js/List.js
@@ -0,0 +1,27 @@
+import { redrawSvg } from "./commonSvgDrawer.js";
+import { images } from "./Images.js";
+import { memes } from "./Memes.js";
+
+const loadListDatas = () => {
+  const contextNode = document.querySelector("#List");
+  const templateNode = contextNode.querySelector("#meme-link-");
+  templateNode.style.display = "none";
+
+  Promise.all([memes.promiseMemes, images.promiseImages]).then((resp) => {
+    resp[0].forEach((m) => {
+      const clonedNode = templateNode.cloneNode(true);
+      clonedNode.style.display = "flex";
+      clonedNode.id += m.id;
+      clonedNode.href += m.id;
+
+      redrawSvg(m, clonedNode.querySelector("svg"),clonedNode.querySelector("svg image"));
+
+      clonedNode.querySelector(".list-titre").innerHTML = m.text;
+      contextNode.appendChild(clonedNode);
+    });
+  });
+};
+
+export function loadList(params) {
+  loadListDatas();
+}
