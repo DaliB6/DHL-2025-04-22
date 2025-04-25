5eb5da08a7af86f94ee0c82e479f4aa38ca19460
champix56 <desqorbaix@free.fr>
2025-04-25 16:36:06 +0200

    chargement differé

--- js/config.js
+++ js/config.js
@@ -1,23 +1,32 @@
 import { loadEditor } from "../vues/editor/editor.js";
 import { loadList } from "../vues/list/list.js";
 
-export const REST_ADR="http://localhost:5679"
+export const REST_ADR = "http://localhost:5679";
 export const routes = [
-    {
-      name: "home",
-      path: "/",
-      templateUrl: "/vues/home/home.html",
+  {
+    name: "home",
+    path: "/",
+    templateUrl: "/vues/home/home.html",
+  },
+  {
+    name: "editor",
+    path: /^\/editor(\/(?<id>\d+))?\/?$/,
+    templateUrl: "/vues/editor/Editor.html",
+    onContentLoaded: loadEditor,
+  },
+  {
+    name: "list",
+    path: /^\/((list)|(thumbnail))\/?$/,
+    templateUrl: "/vues/list/List.html",
+    onContentLoaded: loadList,
+  },
+  {
+    name: "list",
+    path: "/listcanvas",
+    templateUrl: "/vues/list/ListCanvas.html",
+    onContentLoaded: async (route) => {
+      const module = await import("../vues/list/listcanvas.js");
+      module.loadListCanvas(route);
     },
-    {
-      name: "editor",
-      path: /^\/editor(\/(?<id>\d+))?\/?$/,
-      templateUrl: "/vues/editor/Editor.html",
-      onContentLoaded: loadEditor,
-    },
-    {
-      name: "editor",
-      path: /^\/((list)|(thumbnail))\/?/,
-      templateUrl: "/vues/list/List.html",
-      onContentLoaded: loadList,
-    },
-  ];
+  },
+];
