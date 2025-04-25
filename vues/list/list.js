7950a9933290623cb46959551aeb97c6419106dc
champix56 <desqorbaix@free.fr>
2025-04-25 16:22:02 +0200

    async await

--- vues/list/list.js
+++ vues/list/list.js
@@ -21,23 +21,23 @@ function drawSvg(meme, node) {
     imageSvg.remove();
   }
 }
-export function loadList(routeData) {
-  const contextNode =routeData.contextNode.querySelector('#List');
+export const loadList = async (routeData) => {
+  const contextNode = routeData.contextNode.querySelector("#List");
   const aNode = contextNode.querySelector("a");
-  Promise.all([memes.promiseMemes, images.promiseImages]).then((r) => {
-    r[0].forEach((element) => {
-      /**
-       *  @type HTMLElement
-       */
-      const cloned = aNode.cloneNode(true);
-      cloned.id += element.id;
-      cloned.href += element.id;
-      cloned.querySelector("div").innerHTML =
-        element.text.length > 0 ? element.text : "(pas de text)";
-      drawSvg(element, cloned.querySelector("svg"));
-      contextNode.appendChild(cloned);
-    });
-    aNode.style.display = "none";
-    routeData.callback();
+  const prAll = await Promise.all([memes.promiseMemes, images.promiseImages]);
+  prAll[0].forEach((element) => {
+    /**
+     *  @type HTMLElement
+     */
+    const cloned = aNode.cloneNode(true);
+    cloned.id += element.id;
+    cloned.href += element.id;
+    cloned.querySelector("div").innerHTML =
+      element.text.length > 0 ? element.text : "(pas de text)";
+    drawSvg(element, cloned.querySelector("svg"));
+    contextNode.appendChild(cloned);
   });
-}
+  aNode.style.display = "none";
+  routeData.callback();
+return 'chagre de la promise d\'async'
+};
