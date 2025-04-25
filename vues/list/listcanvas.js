0bcccb633e68ea568dbb2b0d4d0aaf55e70d8a6c
champix56 <desqorbaix@free.fr>
2025-04-25 16:56:56 +0200

    test canvas js

--- vues/list/listcanvas.js
+++ vues/list/listcanvas.js
@@ -1,3 +1,6 @@
+import { images } from "../../js/metier/Images.js";
+import { memes } from "../../js/metier/Memes.js";
+
 export const loadListCanvas = async (routeData) => {
   const contextNode = routeData.contextNode.querySelector("#List");
   const aNode = contextNode.querySelector("a");
@@ -8,6 +11,25 @@ export const loadListCanvas = async (routeData) => {
     cloned.href += element.id;
     cloned.querySelector("div").innerHTML =
       element.text.length > 0 ? element.text : "(pas de text)";
+    const img = prAll[1].find((i) => i.id === element.imageId);
+
+    /**
+     * @type HTMLCanvasElement
+     */
+    let cnv = contextNode.querySelector("canvas");
+    cnv.height = img ? img.h : 500;
+    cnv.width = img ? img.w : 500;
+    /**
+     * @type RenderingContext
+     */
+    let ctx = cnv.getContext("2d");
+    ctx.beginPath();
+    ctx.fillStyle = element.color;
+    ctx.font = "bold " + element.fontSize + "px arial";
+    //@ts-ignore
+    ctx.fillText(element.text, element.x, element.y);
+    ctx.fill();
+    ctx.closePath();
     contextNode.appendChild(cloned);
   });
   aNode.style.display = "none";
